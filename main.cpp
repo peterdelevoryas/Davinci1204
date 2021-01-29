9e55452a5c726ddd643eec9a0feea4560ba8755b
peterd <me@peterd.net>
2021-01-29 13:23:11 -0800

    Add sanity test

--- main.cpp
+++ main.cpp
@@ -1,5 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
 
-int main() {
+static char* read_file(const char* path) {
+    auto file = fopen(path, "rb");
+    if (!file)  {
+        printf("Unable to open '%s': %s\n", path, strerror(errno));
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    auto size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    auto text = static_cast<char*>(malloc(size + 1));
+    assert(text);
+
+    assert(fread(text, size, 1, file) == 1);
+    text[size] = 0;
+
+    fclose(file);
+
+    return text;
+}
+
+int main(int argc, char** argv) {
     printf("Hello Davinci1204\n");
+
+    auto text = read_file(argv[1]);
+    printf("%s", text);
+    free(text);
 }
